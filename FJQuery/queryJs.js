67609faae5b254b85a674fad884c09bff0cdbeff
Akashay Anand <akashayanand1@gmail.com>
2022-11-21 00:19:35 +0530

    learning so far... working on JQuery

---
+++ FJQuery/queryJs.js
@@ -0,0 +1,10 @@
+// jQuery or $ can be use interchangabely
+// jQuery('h1').css("color","rgb(123,123,223,0.9)");
+
+// $('h1').css('fontSize','100px');
+
+$('h1').addClass("h1style h2-style");
+
+// alert("hello");
+
+$('h1').removeClass("h2-style");
---
+++ FJS/Dicee Challenge/index.js
@@ -0,0 +1,35 @@
+// alert("working")
+let randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+var randomImages1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImages1);
+
+
+// switch(randomNumber1){
+//     case 1: document.querySelector("img1").setAttribute("src", "images/dice1.png");
+//     case 2: document.querySelector("img1").setAttribute("src", "images/dice1.png");
+//     case 3: document.querySelector("img1").setAttribute("src", "images/dice1.png");
+//     case 4: document.querySelector("img1").setAttribute("src", "images/dice1.png");
+//     case 5: document.querySelector("img1").setAttribute("src", "images/dice1.png");
+//     case 6: document.querySelector("img1").setAttribute("src", "images/dice1.png");
+// }
+let randomNumber2 = Math.floor(Math.random() * 6) +1 ;
+
+var randomImages2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImages2);
+
+// switch(randomNumber2){
+//     case 1: document.querySelector("img2").setAttribute("src", "images/dice1.png");
+//     case 2: document.querySelector("img2").setAttribute("src", "images/dice1.png");
+//     case 3: document.querySelector("img2").setAttribute("src", "images/dice1.png");
+//     case 4: document.querySelector("img2").setAttribute("src", "images/dice1.png");
+//     case 5: document.querySelector("img2").setAttribute("src", "images/dice1.png");
+//     case 6: document.querySelector("img2").setAttribute("src", "images/dice1.png");
+// }
+
+if(randomNumber1 > randomNumber2)
+    document.querySelector("h1").innerHTML="🏆Player 1 Wins!"; 
+else if(randomNumber1 < randomNumber2)
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🏆";
+else    document.querySelector("h1").innerHTML = "Draw!";
