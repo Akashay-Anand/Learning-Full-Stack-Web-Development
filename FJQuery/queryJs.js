9df054ad08cc093fa47dd5b83b3f2e9f80955e15
Akashay Anand <akashayanand1@gmail.com>
2022-11-25 16:53:38 +0530

    update

--- FJQuery/queryJs.js
+++ FJQuery/queryJs.js
@@ -10,5 +10,38 @@ $('h1').addClass("h1style h2-style");
 $('h1').removeClass("h2-style");
 
 console.log($('h1').hasClass('h1style'));
-console.log($('img').attr("src"));
+console.log($('img').attr("src")); 
 $('a').attr("href","https://google.com").attr("target","_blank");
+
+// Event Listeners jQuery
+// Note: in 
+
+// H1
+$('h1').click(function() {
+    $('h1').css('color','red');
+});
+
+// button 
+// with JS
+for(var i = 0; i<5; i++){
+    document.querySelectorAll('button')[i].addEventListener("click", function () {
+        document.querySelector('h1').style.color = 'darkorange';
+    });
+}
+// with jQuery
+$('button').click(function() {
+    // document.querySelector('h1').classList.toggle('h1style'); 
+    // $('h1').css("color","green").toggle("color");
+});
+
+// Key Strocks
+// this will target the input box only
+// $("input").keypress(function(event){
+//     console.log(event.key);
+// }); 
+$(document).keypress(function(event){
+    console.log(event.key);
+    // $('h1').text(event.key)
+})
+
+ 
